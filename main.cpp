b986deccf41c1ba59ece1633f7beedc4f9379cd0
Pegaster <pegaster@MacBook-Vanya.local>
2020-11-19 15:31:51 +0300

    Реализовал перемещение по локациям

--- main.cpp
+++ main.cpp
@@ -3,14 +3,21 @@
 
 #define MAP_HEIGHT 22
 #define MAP_WIDTH 44
+#define LEFT_MAP 2
+#define RIGHT_MAP 3
+#define UP_MAP 0
+#define DOWN_MAP 1
 #define FLOOR '#'
 #define DRAW_FLOOR ' '
 #define STICK 'I'
 using namespace std;
 
 char map[MAP_HEIGHT][MAP_WIDTH];
-
-string map_name("1.bin");
+short exits = 4;
+string current_map = "1.bin";
+string maps[4] = {"1.bin"};
+bool isDirectionUsed[4];
+int map_number = 0;
 bool game = true;
 short dir = 0;
 
@@ -54,9 +61,41 @@ struct character{
 };
 
 character player = {spawn_y, spawn_y, 'O', FLOOR, 100, false};
+
 void loadMap(){
+    char exit_type;
+    for(int i = 0; i < 4; i++){
+        isDirectionUsed[i] = false;
+    }
+
     ifstream map_file;
-    map_file.open(map_name);
+    map_file.open(maps[map_number]);
+    map_file >> exits;
+    for(int i = 0; i < exits; i++){
+        int index = 0;
+        map_file >> exit_type;
+        switch (exit_type)
+        {
+        case 'U':
+            index = UP_MAP;
+            break;
+
+        case 'D':
+            index = DOWN_MAP;
+            break;
+
+        case 'L':
+            index = LEFT_MAP;
+            break;
+
+        case 'R':
+            index = RIGHT_MAP;
+            break;
+
+        }
+        map_file >> maps[index]; 
+        isDirectionUsed[index] = true;
+    }
     for(int y = 0; y < MAP_WIDTH - 1; y++){
         map_file >> map[y];
     }
@@ -146,8 +185,6 @@ void input(){
         case 'k':
             showPosition = !showPosition;
             break;
-        case 't':
-            player.attack = true;
         case 'g':
             char buffer = player.weapoon;
             player.getWeapoon();
@@ -165,6 +202,20 @@ void logic(){
                     break;
             }
         }
+        else if((player.y == 0) && (dir == -1)){            
+            if(isDirectionUsed[UP_MAP]){
+                map_number = UP_MAP;
+                player.goToY(MAP_HEIGHT - 1);
+                loadMap();
+            }
+        }
+        else if((player.y == MAP_HEIGHT - 1) && (dir == 1) && isDirectionUsed[DOWN_MAP]){
+            map_number = DOWN_MAP;
+            player.goToY(0);
+            loadMap();
+            
+        }
+
     }
     else{
         dir -= 3;
@@ -175,6 +226,18 @@ void logic(){
                     player.moveX(dir);
                     break;
             }
+        }
+        else if((player.x == MAP_WIDTH - 1) && (dir == 1) && isDirectionUsed[RIGHT_MAP]){
+            map_number = RIGHT_MAP;
+            player.goToX(0);
+            loadMap();
+            
+        }
+        else if((player.x == 0) && (dir == -1) && isDirectionUsed[LEFT_MAP]){
+            map_number = LEFT_MAP;
+            player.goToX(MAP_WIDTH - 1);
+            loadMap();
+            
         }
     }
     dir = 0;
